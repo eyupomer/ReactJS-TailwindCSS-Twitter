a1a4d857172603e11e9e75908e838dc984829796
eyupomer <odabasieyupomer@gmail.com>
2022-11-17 15:52:47 +0300

    Icons done

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {
+    HomeIcon,
+    ExploreIcon,
+    NotificationsIcon,
+    MessagesIcon,
+    BookmarksIcon,
+    ListsIcon,
+    ProfileIcon,
+    MoreIcon
+} from '../icons/Icon';
 
 const Sidebar = () => {
-  return (
-    <div className="w-72 bg-blue-200">Sidebar</div>
-  )
+    return (
+        <div className="w-72 bg-blue-200">
+            Sidebar 
+            <HomeIcon/>
+            <ExploreIcon/>
+            <NotificationsIcon/>
+            <MessagesIcon/>
+            <BookmarksIcon/>
+            <ListsIcon/>
+            <ProfileIcon/>
+            <MoreIcon/>
+            </div>
+    )
 }
 
 export default Sidebar
